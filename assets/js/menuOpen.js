1749bf84893f59aab1f5f3f15f02fe125e24c006
Rogix <12768403+rogix@users.noreply.github.com>
2023-07-31 21:33:21 -0300

    Update navbar

--- assets/js/menuOpen.js
+++ assets/js/menuOpen.js
@@ -3,5 +3,6 @@ export default function menuOpen() {
     const burgerButton = document.querySelector('.gh-burger');
     burgerButton.addEventListener('click', function () {
         document.body.classList.toggle('gh-head-open');
+        document.getElementById('gh-head').classList.toggle('gh-head-opened');
     });
 }
